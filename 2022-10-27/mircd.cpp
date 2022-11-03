c46cc688672c50d6f2ddc6ede81133fb788d8b9a
Sean Wei <me@sean.taipei>
2022-11-03 12:48:42 +0800

    mircd: Extract sendsf

--- 2022-10-27/mircd.cpp
+++ 2022-10-27/mircd.cpp
@@ -8,9 +8,8 @@ Channel chans[MAX_CHANS];
 int main(int argc, char *argv[]) {
 	struct sockaddr_in srvAddr, cliAddr;
 	socklen_t addr_size = sizeof(cliAddr);
-	int srvSock, uid, cid, n, one=1, cnt=0, k=0;
-	char *cmd, *arg, args[42][MAX_TEXT], buf[3072], space[2]=" ";
-	time_t now; time(&now);
+	int srvSock, uid, cid, n, i, one=1, cnt=0, muid=0;
+	char lines[42][MAX_TEXT], cmd[MAX_TEXT], arg[MAX_TEXT], args[42][MAX_TEXT], buf[3072], *ptr;
 	struct timeval zero = {0, 0};
 	fd_set fds;
 
@@ -37,23 +36,22 @@ int main(int argc, char *argv[]) {
 	}
 	listen(srvSock, 42);
 
-	for (;;) {
+	for (;;) {  // Main loop
 		FD_ZERO(&fds); FD_SET(srvSock, &fds);
 		while (select(1024, &fds, NULL, NULL, &zero) > 0) {
-			users[++k].sock = accept(srvSock, (struct sockaddr*) &cliAddr, &addr_size);
-			if (users[k].sock < 0) {
+			users[++muid].sock = accept(srvSock, (struct sockaddr*) &cliAddr, &addr_size);
+			if (users[muid].sock < 0) {
 				fprintf(stderr, "%s: (%d) Accept error\n", argv[0], errno);
-				--k;
+				--muid;
 				usleep(100 * 1000);
 				break;
 			}
 			cnt++;
 
-			sprintf(users[k].addr, "%s", inet_ntoa(cliAddr.sin_addr));
-			strcpy(users[k].name, animals[(now * 401 + k * 263) % 877]);
-			time(&users[k].created_at);
+			sprintf(users[muid].addr, "%s", inet_ntoa(cliAddr.sin_addr));
+			time(&users[muid].created_at);
 
-			printf("* client connected from %s\n", users[k].addr);
+			printf("* client connected from %s\n", users[muid].addr);
 		}
 
 		for (uid=1; uid<MAX_USERS; uid++) {
@@ -64,84 +62,208 @@ int main(int argc, char *argv[]) {
 				continue;
 
 			if ((n = read(users[uid].sock, buf, 3070)) == 0) {
-				sendaf(uid, "User <%s> has left the server\n", users[uid].name);
 				printf("* client %s disconnected\n", users[uid].addr);
 				time(&users[uid].deleted_at);
 				close(users[uid].sock);
 				cnt--;  // Current user count
 				continue;
 			}
-			buf[n-1] = '\0';  // newline -> eol
+			buf[n-1] = '\0';
 
-			cmd = strtok(buf, space);
-			arg = strtok(nullptr, space);
-			// TODO: arg -> args
-			strcpy(args[0], arg);
-			if (strcmp(cmd, "NICK") == 0) {
-				// TODO: Check USER command
-				strcpy(users[uid].name, arg);
-				sendaf(uid, "*** User <%s> joined the server\n", users[uid].name);
-				sendf(uid, ":mircd 001 %s :Welcome to the minimized IRC daemon!\n", users[uid].name);
-				sendf(uid, ":mircd 251 %s :There are %d users and 0 invisible on 1 server\n", users[uid].name, cnt);
-				sendf(uid, ":mircd 375 %s :- mircd Message of the day -\n", users[uid].name);
-				sendf(uid, ":mircd 372 %s :-  Hello, World!\n", users[uid].name);
-				sendf(uid, ":mircd 372 %s :-               @                    _\n", users[uid].name);
-				sendf(uid, ":mircd 372 %s :-   ____  ___   _   _ _   ____.     | |\n", users[uid].name);
-				sendf(uid, ":mircd 372 %s :-  /  _ `'_  \\ | | | '_/ /  __|  ___| |\n", users[uid].name);
-				sendf(uid, ":mircd 372 %s :-  | | | | | | | | | |   | |    /  _  |\n", users[uid].name);
-				sendf(uid, ":mircd 372 %s :-  | | | | | | | | | |   | |__  | |_| |\n", users[uid].name);
-				sendf(uid, ":mircd 372 %s :-  |_| |_| |_| |_| |_|   \\____| \\___,_|\n", users[uid].name);
-				sendf(uid, ":mircd 372 %s :-  minimized internet relay chat daemon\n", users[uid].name);
-				sendf(uid, ":mircd 372 %s :-\n", users[uid].name);
-				sendf(uid, ":mircd 376 %s :End of message of the day\n", users[uid].name);
-			} else if (strcmp(cmd, "LIST") == 0) {
-				// TODO: Specify channel
-				sendf(uid, ":mircd 321 %s Channel :Users Name\n", users[uid].name);
-				for (int i=0; i<MAX_CHANS; i++) {
-					if (!chans[i].created_at || chans[i].deleted_at)
-						continue;
-					sendf(uid, ":mircd 322 %s %s %s :%s\n", users[uid].name, chans[i].name, chans[i].cnt, chans[i].topic);
-				}
-				sendf(uid, ":mircd 323 %s :End of List\n", users[uid].name);
-			} else if (strcmp(cmd, "JOIN") == 0) {
-				for (cid=1; cid<MAX_CHANS; cid++)
-					if (strcmp(arg, chans[cid].name) == 0)
-						break;
-				if (cid == MAX_CHANS) {  // Channel not found
-					for (cid=1; cid<MAX_CHANS; cid++)
-						if (!chans[cid].created_at)
+			ptr = strtok(buf, "\r\n");
+			for (i=0; ptr != nullptr; i++) {
+				if (ptr[0])
+					strcpy(lines[i], ptr);
+				else
+					i--;
+				ptr = strtok(nullptr, "\r\n");
+			}
+			lines[i][0] = '\0';
+
+			for (int ln=0; lines[ln][0]; ln++) {
+				// Parse command
+				strcpy(buf, lines[ln]);
+				ptr = strtok(buf, " ");
+				strcpy(cmd, ptr);
+				ptr = strtok(nullptr, "");
+				if (ptr == nullptr) {
+					strcpy(arg, "");
+					args[0][0] = '\0';
+				} else {
+					strcpy(arg, ptr);
+					strcpy(buf, arg);
+					for (i=0; ; i++) {
+						if (buf[0] == ':') {
+							strcpy(args[i], buf+1);
+							args[i+1][0] = '\0';
 							break;
-					time(&chans[cid].created_at);
-					strcpy(chans[cid].name, arg);
-				}
-				for (int i=MAX_USERS-1; i>1; i--)  // Append user list
-					if (chans[cid].users[i-1] && !chans[cid].users[i]) {
-						chans[cid].users[i] = uid;
-						break;
+						}
+						ptr = strtok(buf, " ");
+						strcpy(args[i], ptr);
+						ptr = strtok(nullptr, "");
+						if (ptr == nullptr) {
+							args[i+1][0] = '\0';
+							break;
+						}
+						strcpy(buf, ptr);
+					}
+				}  // Parse command
+
+				if (strcmp(cmd, "PING") == 0) {
+					sendf(uid, "PONG %s\n", arg);
+				} else if (strcmp(cmd, "NICK") == 0) {
+					strcpy(users[uid].name, args[0]);
+				} else if (strcmp(cmd, "USER") == 0) {
+					sendsf(uid, 0x1, ":Welcome to the minimized IRC daemon!");
+					sendsf(uid, 251, ":There are %d users and 0 invisible on 1 server", cnt);
+					sendsf(uid, 375, ":- mircd Message of the day -");
+					sendsf(uid, 372, ":-  Hello, World!");
+					sendsf(uid, 372, ":-               @                    _");
+					sendsf(uid, 372, ":-   ____  ___   _   _ _   ____.     | |");
+					sendsf(uid, 372, ":-  /  _ `'_  \\ | | | '_/ /  __|  ___| |");
+					sendsf(uid, 372, ":-  | | | | | | | | | |   | |    /  _  |");
+					sendsf(uid, 372, ":-  | | | | | | | | | |   | |__  | |_| |");
+					sendsf(uid, 372, ":-  |_| |_| |_| |_| |_|   \\____| \\___,_|");
+					sendsf(uid, 372, ":-  minimized internet relay chat daemon");
+					sendsf(uid, 372, ":-");
+					sendsf(uid, 376, ":End of message of the day");
+				} else if (strcmp(cmd, "JOIN") == 0) {
+					if (args[0][0] == '\0') {
+						sendsf(uid, 461, "JOIN :Not enought parameters");
+						continue;;
+					}
+					strcpy(buf+1, args[0] + (args[0][0] == '#' ? 1 : 0));
+					buf[0] = '#';  // Prepend #hash if not exist
+
+					for (cid=1; cid<MAX_CHANS; cid++)
+						if (strcmp(buf, chans[cid].name) == 0)
+							break;  // Channel found
+					if (cid == MAX_CHANS) {  // Channel not found
+						for (cid=1; cid<MAX_CHANS; cid++)
+							if (!chans[cid].created_at)
+								break;  // Empty slot
+						time(&chans[cid].created_at);
+						strcpy(chans[cid].name, buf);
+					}
+					for (i=MAX_USERS-1; i>0; i--)  // Append user list
+						if (chans[cid].users[i-1] && !chans[cid].users[i]) {
+							chans[cid].users[i] = uid;
+							chans[cid].cnt++;
+							break;
+						}
+
+					sendf(uid, ":%s JOIN %s\n", users[uid].name, chans[cid].name);
+					if (chans[cid].topic[0] == '\0')
+						sendsf(uid, 331, "%s :No topic is set", chans[cid].name);
+					else
+						sendsf(uid, 332, "%s :%s", chans[cid].name, chans[cid].topic);
+
+					// List channel users
+					sendf(uid, ":mircd 353 %s %s :", users[uid].name, chans[cid].name);
+					for (i=1; i<MAX_USERS; i++) {
+						if (!chans[cid].users[i])
+							continue;
+						sendf(uid, "%s ", users[i].name);
+					}
+					sendf(uid, "\n");
+					sendsf(uid, 366, "%s :End of Names List", chans[cid].name);
+				} else if (strcmp(cmd, "TOPIC") == 0) {
+					if (args[0][0] == '\0') {
+						sendsf(uid, 461, "TOPIC :Not enought parameters");
+						continue;
+					}
+					strcpy(buf+1, args[0] + (args[0][0] == '#' ? 1 : 0));
+					buf[0] = '#';  // Prepend #hash if not exist
+
+					for (cid=1; cid<MAX_CHANS; cid++)
+						if (strcmp(buf, chans[cid].name) == 0)
+							break;  // Channel found
+					if (cid == MAX_CHANS) {
+						sendsf(uid, 442, "%s :You are not on that channel", buf);
+						continue;
 					}
 
-				sendf(uid, ":%s JOIN %s\n", users[uid].name, chans[cid].name);
-				if (chans[cid].topic[0] == '\0')
-					sendf(uid, ":mircd 331 %s %s :No topic is set\n", users[uid].name, chans[cid].name);
-				else
-					sendf(uid, ":mircd 332 %s %s :%s\n", users[uid].name, chans[cid].name, chans[cid].topic);
-
-				// List channel users
-				sendf(uid, ":mircd 353 %s %s :", users[uid].name, chans[cid].name);
-				for (int i=1; i<MAX_USERS; i++) {
-					if (!chans[cid].users[i])
+					for (i=1; i<MAX_USERS; i++)
+						if (chans[cid].users[i] == uid)
+							break;  // User found
+					if (i == MAX_USERS) {
+						sendsf(uid, 442, "%s :You are not on that channel", buf);
 						continue;
-					sendf(uid, "%s ", users[i].name);
+					}
+
+					if (args[1][0])  // Set topic
+						strcpy(chans[cid].topic, args[1]);
+					if (chans[cid].topic[0] == '\0')  // Show topic
+						sendsf(uid, 331, "%s :No topic is set", chans[cid].name);
+					else
+						sendsf(uid, 332, "%s :%s", chans[cid].name, chans[cid].topic);
+				} else if (strcmp(cmd, "PRIVMSG") == 0) {
+					if (args[0][0] == '\0') {
+						sendsf(uid, 411, ":No recipient given (PRIVMSG)");
+						continue;
+					}
+					if (args[1][0] == '\0') {
+						sendsf(uid, 412, ":No text to send");
+						continue;
+					}
+
+					for (cid=1; cid<MAX_CHANS; cid++)
+						if (strcmp(args[0], chans[cid].name) == 0)
+							break;  // Channel found
+					if (cid == MAX_CHANS) {
+						sendsf(uid, 401, "%s :No such nick/channel", args[0]);
+						continue;
+					}
+					for (i=1; i<MAX_USERS; i++)
+						if (chans[cid].users[i] && i != uid)
+							sendf(i, ":%s PRIVMSG %s :%s\n", users[uid].name, chans[cid].name, args[1]);
+				} else if (strcmp(cmd, "PART") == 0) {
+					if (args[0][0] == '\0') {
+						sendsf(uid, 461, "PART :Not enought parameters");
+						continue;
+					}
+
+					for (cid=1; cid<MAX_CHANS; cid++)
+						if (strcmp(args[0], chans[cid].name) == 0)
+							break;  // Channel found
+					if (cid == MAX_CHANS) {
+						sendsf(uid, 403, "%s :No such channel", args[0]);
+						continue;
+					}
+					for (i=1; i<MAX_USERS; i++)
+						if (chans[cid].users[i] == uid)
+							break;  // User found
+					if (i == MAX_USERS) {
+						sendsf(uid, 442, "%s :You are not on that channel", buf);
+						continue;
+					}
+					chans[cid].users[i] = 0;
+					chans[cid].cnt--;
+					sendf(uid, ":%s PART :%s\n", users[uid].name, chans[cid].name);
+				} else if (strcmp(cmd, "USERS") == 0) {
+					sendsf(uid, 392, ":%-32s Terminal  Host", "UserID");
+					for (i=0; i<MAX_USERS; i++) {
+						if (!users[i].created_at || users[i].deleted_at)
+							continue;
+						sendsf(uid, 393, ":%-32s %-9s %s", users[i].name, "-", users[i].addr);
+					}
+					sendsf(uid, 394, ":End of users");
+				} else if (strcmp(cmd, "LIST") == 0) {
+					sendsf(uid, 321, "Channel :Users Name");
+					for (i=0; i<MAX_CHANS; i++) {
+						if (!chans[i].created_at || chans[i].deleted_at)
+							continue;
+						if (args[0][0] && strcmp(args[0], chans[i].name) != 0)
+							continue;
+						sendsf(uid, 322, "%s %d :%s", chans[i].name, chans[i].cnt, chans[i].topic);
+					}
+					sendsf(uid, 323, ":End of List");
+				} else if (strcmp(cmd, "QUIT") == 0) {
+					// pass
+				} else {
+					sendsf(uid, 421, "%s :Unknown command", cmd);
 				}
-				sendf(uid, "\n");
-				sendf(uid, ":mircd 366 %s %s :End of Names List\n", users[uid].name, chans[cid].name);
-			} else if (strcmp(cmd, "TOPIC") == 0) {
-				// TODO
-			} else if (strcmp(cmd, "PING") == 0) {
-				sendf(uid, "PONG %s\n", arg);
-			} else {
-				sendf(uid, ":mircd 421 %s %s :Unknown command\n", users[uid].name, cmd);
-			}
+			}  // One line of command
 		}  // End for users
 	}  // End main loop
 }  // main function
@@ -157,23 +279,15 @@ int sendf(const int uid, const char *fmt, ...) {
 	return send(users[uid].sock, buf, strlen(buf), 0);
 }  // sendf
 
-void sendaf(const int uid, const char *fmt, ...) {
-	struct tm *timeinfo;
-	char buf[4096];
-	time_t now;
+int sendsf(const int uid, const int code, const char *fmt, ...) {
+	char new_fmt[8192], buf[8192];
 	va_list va;
 
-	time(&now);
-	timeinfo = localtime(&now);
-	strftime(buf, 21, "%F %T ", timeinfo);
+	sprintf(new_fmt, ":mircd %03d %s %s\n", code, users[uid].name, fmt);
 
-	va_start(va, fmt);
-	vsprintf(buf+20, fmt, va);
+	va_start(va, new_fmt);
+	vsprintf(buf, new_fmt, va);
 	va_end(va);
 
-	for (int i=1; i<MAX_USERS; i++) {
-		if (!users[i].created_at || users[i].deleted_at || i == uid)
-			continue;
-		send(users[i].sock, buf, strlen(buf), 0);
-	}
-}  // sendaf
+	return send(users[uid].sock, buf, strlen(buf), 0);
+}  // sendsf
